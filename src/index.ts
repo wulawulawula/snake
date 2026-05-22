69ce458b8c4588da293a358ffd7b7577fe634a2a
oulaoulaoula <m17376757163@163.com>
2026-05-22 11:10:21 +0800

    commit -m 'food类实现‘

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 // 引入样式
 import './style/index.less'
 
+// 公共方法
+// 判断两个数组是否完全一致
+function arrEqual<T>(arr1:T[],arr2:T[]):boolean{
+    return JSON.stringify(arr1) === JSON.stringify(arr2)
+}
 // 定义食物类food
 
 class Food {
@@ -20,6 +25,11 @@ class Food {
         // 生成随机位置
         const left = Math.round(Math.random() * 29) * 10
         const top = Math.round(Math.random() * 29) * 10
+        if(arrEqual(this.XY, [left, top])){
+            // 如果生成的位置和当前食物的位置一样，则重新生成
+            this.change()
+            return
+        }
         this.element.style.left = left + 'px'
         this.element.style.top = top + 'px'
         console.log(this.XY)
