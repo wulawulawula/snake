5b657f6e58cd9849a3f3daef5c1d3933ae4d8e11
oulaoulaoula <m17376757163@163.com>
2026-05-22 10:59:33 +0800

    commit -m 'food类实现‘

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
 // 引入样式
 import './style/index.less'
-console.log(132)
+
+// 定义食物类food
+
+class Food {
+    element:HTMLElement;
+
+    constructor(){
+        // 获取food的dom元素
+        this.element = document.getElementById('food')!;
+    }
+
+    // 获取食物的XY坐标
+    get XY(){
+        return [this.element.offsetLeft, this.element.offsetTop]
+    }
+
+    change(){
+        // 生成随机位置
+        const left = Math.round(Math.random() * 29) * 10
+        const top = Math.round(Math.random() * 29) * 10
+        this.element.style.left = left + 'px'
+        this.element.style.top = top + 'px'
+        console.log(this.XY)
+    }
+}
+
+const food = new Food()
+console.log(food.XY)
+food.change()
